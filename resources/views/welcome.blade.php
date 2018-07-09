cc1ff16a4e85d572598a43d1049fe399fab4efe2
tyler <tyler.github2018@gmail.com>
2018-07-09 01:57:04 +0000

    [add] background video [edit] welcomeblade. COMPLETE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 <!-- 
 TODO list 
 
-    背景に動画ながす(今回は画像で対応）
-    サービス名を中心に表示する
-    サインイン・ログインボタンを左右にサービス名のしたに表示する
+    x 背景に動画ながす
+    x サービス名を中心に表示する
+    x サインイン・ログインボタンを左右にサービス名のしたに表示する
     x レイアウトを作成する
+    x ボタンの加工
+    
+    終わってないこと
+    携帯画面でのアスペクト
 
 -->
 
@@ -17,7 +21,25 @@ TODO list
     <!-- welcome blade の表示するプログラム --> 
     
     
-    <img src="pictures/welcomeblade.JPG" alt="welcomeblade"> 
+    
+    <!-- サービス名表示 -->
+    <div class="title">
+        <h1 clsss="headings">Smoky Doukies</h1>
+        
+            <div>
+                <a href= "#" class="buttons">Sign up</a>
+                <a href= "#" class="buttons">Log in</a>
+            </div>
+        
+    </div>
+    
+    
+    <!--- srcのところに動画を入れるだけで変更可能 -->
+    
+    <video src="video/test.mp4" muted autoplay loop></video>
+    
+    
+
     
     
     
