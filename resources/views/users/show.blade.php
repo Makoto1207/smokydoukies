98ca597a08b9cd0cece1ea7fb179741c6c5eed19
Makoto Hasegawa <ec2-user@ip-172-31-34-84.us-east-2.compute.internal>
2018-07-09 06:20:22 +0000

    tweet

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -17,6 +17,18 @@
                 <li><a href="#">TimeLine</a></li>
                 
             </ul>
+            
+             @if (Auth::id() == $user->id)
+                  {!! Form::open(['route' => 'microposts.store']) !!}
+                      <div class="form-group">
+                          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                          {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+                      </div>
+                  {!! Form::close() !!}
+            @endif
+            @if (count($microposts) > 0)
+                @include('microposts.microposts', ['microposts' => $microposts])
+            @endif
         </div>
     </div>
 @endsection
