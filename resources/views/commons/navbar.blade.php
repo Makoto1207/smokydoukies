e1072e389bcf7a2f25603121e2fee13c2f9a3908
yuna <ec2-user@ip-172-31-10-122.us-east-2.compute.internal>
2018-07-10 06:53:08 +0000

    [edit] profile.blade navbar

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -22,7 +22,7 @@
     <div class="collapse navbar-collapse navbar-fixed-top" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                     <span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
                 </a>
                 <!--　ホームボタン　-->
