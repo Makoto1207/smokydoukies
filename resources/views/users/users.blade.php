3fb90c5cb4f2f36ed372f6afa0e9fe3c733ed186
ninomy <ec2-user@ip-172-31-11-6.us-east-2.compute.internal>
2018-07-13 05:37:33 +0000

    [edit] tweet no gazou no

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -6,7 +6,7 @@
 @foreach ($users as $user)
    
 
-    <div class="col-md-3">
+    <div class="col-md-4">
         <div class="img-thumbnail" id="profile_pic">
             <img  src="{{secure_asset('/images/cigs/'. $user->brand. '.jpg')}}"  alt="">
             <p class="nickname">{!! link_to_route('users.show', $user->nickname, ['id' => $user->id]) !!}</p>
