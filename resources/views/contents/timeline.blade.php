3a5895d2386b18621bc1e96d5550af18f516fc24
ninomy <ec2-user@ip-172-31-11-6.us-east-2.compute.internal>
2018-07-17 04:39:03 +0000

    [edit] profile title

--- resources/views/contents/timeline.blade.php
+++ resources/views/contents/timeline.blade.php
@@ -10,11 +10,9 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <h2>Timeline</h2>
-            <div class="panel-body">
                 @if (count($microposts) > 0)
                     @include('microposts.microposts', ['microposts' => $microposts])
                 @endif
-            </div>
         </div>     
     </div>
 </div>
