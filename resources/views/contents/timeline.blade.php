a18639ff391c8f7acbe667cc81967aaa6c2a926e
ninomy <ec2-user@ip-172-31-11-6.us-east-2.compute.internal>
2018-07-17 03:59:59 +0000

    [edit] timeline

--- resources/views/contents/timeline.blade.php
+++ resources/views/contents/timeline.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.app')
 
-@section('content')
 
-    <!-- ツイートを表示するためまmicropost blade 私用 -->
-    @if (count($microposts) > 0)
-        @include('microposts.microposts', ['microposts' => $microposts])
-    @endif
+
+
+@section('content')
     
 
-@endsection
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <h2>Timeline</h2>
+            <div class="panel-body">
+                @if (count($microposts) > 0)
+                    @include('microposts.microposts', ['microposts' => $microposts])
+                @endif
+            </div>
+        </div>     
+    </div>
+</div>
+
+
+@endsections
