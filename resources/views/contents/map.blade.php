59d8110d84b99556bde613c50619500c24020988
sophia <ec2-user@ip-172-31-0-202.us-east-2.compute.internal>
2018-07-20 05:43:18 +0000

    [edit] not completed

--- resources/views/contents/map.blade.php
+++ resources/views/contents/map.blade.php
@@ -16,53 +16,53 @@
     <div class="titleformap"><h2>Smoking areas in Futakotamagawa</h2></div>
 
 <div class="container" style="margin-top: 70px;">
-    <h2>Click on map for more details!</h2>
+    <h3>Click on map for more details! ↓↓</h3>
 
-     
-    <div class="introduction">
-        <img src="{{secure_asset('/img/mapFULLedit2.jpg')}}" alt="sample" usemap="#sample">
+    <div class="col-100 col-offset-1">
+        <div class="introduction">
+            <img src="{{secure_asset('/img/mapFULLedit2.jpg')}}" alt="sample" usemap="#sample" class='img-responsive'>
+            
+            <map name="sample">
+                <area shape="poly" coords="100,30,76,178,94,200,345,299,396,157" href="#imageA" alt="map_a">
+                <area shape="poly" coords="98,213,60,234,38,331,201,414,233,357,252,266" href="#imageB" alt="map_b">
+                <area shape="poly" coords="287,300,240,411,398,515,454,389" href="#imageC" alt="map_c">
+                <area shape="poly" coords="502,442,451,558,556,669,696,569"href="#imageD" alt="map_d">
+            </map>
+        </div>
         
-        <map name="sample">
-            <area shape="poly" coords="100,30,76,178,94,200,345,299,396,157" href="#imageA" alt="map_a">
-            <area shape="poly" coords="98,213,60,234,38,331,201,414,233,357,252,266" href="#imageB" alt="map_b">
-            <area shape="poly" coords="287,300,240,411,398,515,454,389" href="#imageC" alt="map_c">
-            <area shape="poly" coords="502,442,451,558,556,669,696,569"href="#imageD" alt="map_d">
-        </map>
+        <div class="mapdetail">
+            
+            
+            <div class="sectiontitle"><h2 id=imageA>A</h2></div>
+                <div class="maplarge">
+                    <img src="img/mapAedit.jpg" width="550" height="550" class='img-responsive'>
+                </div>
+                    <a href="{{ route('store')}}" class="storebuttons">Stores</a>
+                
+            
+            
+            <div class="sectiontitle"><h2 id=imageB>B</h2></div>
+                <div class="maplarge">
+                    <img src="img/mapBedit.jpg" width="550" height="550" class='img-responsive'>
+                </div>
+                    <a href="{{ route('store')}}" class="storebuttons">Stores</a>
+                
+            
+            
+            <h2 id=imageC>C</h2>
+                <div class="maplarge">
+                    <img src="img/mapCedit.jpg" width="550" height="550" class='img-responsive'>
+                </div>
+                    <a href="{{ route('store')}}" class="storebuttons">Stores</a>
+            
+            
+            
+            <h2 id=imageD>D</h2>
+                <div class="maplarge">
+                    <img src="img/mapDedit.jpg" width="550" height="550" class='img-responsive'>
+                </div>
+                    <a href="{{ route('store')}}" class="storebuttons">Stores</a>
+                
+        </div>
     </div>
-    
-    <div class="mapdetail">
-        
-        
-        <div class="sectiontitle"><h2 id=imageA>A</h2></div>
-            <div class="maplarge">
-                <img src="img/mapAedit.jpg" width="550" height="550">
-            </div>
-                <a href="{{ route('store')}}" class="storebuttons">Stores</a>
-            
-        
-        
-        <div class="sectiontitle"><h2 id=imageB>B</h2></div>
-            <div class="maplarge">
-                <img src="img/mapBedit.jpg" width="550" height="550">
-            </div>
-                <a href="{{ route('store')}}" class="storebuttons">Stores</a>
-            
-        
-        
-        <h2 id=imageC>C</h2>
-            <div class="maplarge">
-                <img src="img/mapCedit.jpg" width="550" height="550">
-            </div>
-                <a href="{{ route('store')}}" class="storebuttons">Stores</a>
-        
-        
-        
-        <h2 id=imageD>D</h2>
-            <div class="maplarge">
-                <img src="img/mapDedit.jpg" width="550" height="550">
-            </div>
-                <a href="{{ route('store')}}" class="storebuttons">Stores</a>
-            
-    </div>
-    
 @endsection
