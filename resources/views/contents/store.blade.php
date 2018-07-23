f9e06c709297b104e771fe15f6940563cc1bdc8b
gaku.murakami <cm150205@g.hit-u.ac.jp>
2018-07-23 05:13:52 +0000

    [add] search button on store.blade.php

--- resources/views/contents/store.blade.php
+++ resources/views/contents/store.blade.php
@@ -11,13 +11,13 @@
         <div class="col-md-8 col-md-offset-2">
         
         <span id="search_message">Push the button to update the restaurant list　
-        <br>  Area : <a href="{{ route('store')}}" id="search_message2">All</a> ( <a href="{{ route('store')}}?name=(A)" id="search_message2">(A)</a> | <a href="{{ route('store')}}?name=(B)" id="search_message2">(B)</a> | <a href="{{ route('store')}}?name=(C)" id="search_message2">(C)</a> | <a href="{{ route('store')}}?name=(D)" id="search_message2">(D)</a> )</span>
-         
+        <br>  Area : <a href="{{ route('store')}}" id="search_message2">All</a> ( <a href="{{ route('store')}}?name=(A)" id="search_message2">A</a> | <a href="{{ route('store')}}?name=(B)" id="search_message2">B</a> | <a href="{{ route('store')}}?name=(C)" id="search_message2">C</a> | <a href="{{ route('store')}}?name=(D)" id="search_message2">D</a> )
+        <br>  Type : <a href="{{ route('store')}}?name=禁煙" id="search_message2">禁煙</a> | <a href="{{ route('store')}}?name=分煙" id="search_message2">分煙</a> | <a href="{{ route('store')}}?name=喫煙可"  id="search_message2">喫煙可</a></span>
         
         <table class="table table-striped">
             <thead>
               <tr id="store_all">
-                <th>Restaurant（ <a href="{{ route('store')}}?name=禁煙" id="search_message2">禁煙</a> | <a href="{{ route('store')}}?name=分煙" id="search_message2">分煙</a> | <a href="{{ route('store')}}?name=喫煙可" id="search_message2">喫煙可</a> ）</th>
+                <th>Restaurant</th>
                 <th>Area</th>
                 <th>Type</th>
               </tr>
